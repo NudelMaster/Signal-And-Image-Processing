55031ebc3e8a331052c6de8509dc4886e394865e
NudelMaster <yefimn@mail.tau.ac.il>
2026-06-09 19:00:00 +0300

    removed main.py since its not required

--- main.py
+++
@@ -1,9 +0,0 @@
-"""Entry point for running all the scripts."""
-import numpy as np
-# Set the random seed for reproducibility
-np.random.seed(0)
-
-
-def runme_desnoising():
-    """Run the desnoising script."""
-    
